fa87ca321cb5aa0970ddfbe286c1f30cfa4c54c0
ColiJD <colindresj9@gmail.com>
2024-08-12 14:47:26 -0600

    Creando carpetas temporales

--- MediaDownloader.py
+++ MediaDownloader.py
@@ -1,22 +1,12 @@
 import streamlit as st
 import os
 from funciones import setup_session_state, validate_url_and_format, handle_download, check_and_warn_changes, YouTubeDownloader
+from pathlib import Path
 
 def main():
     st.title("Descargador de Videos de YouTube")
     # Inicializar el estado de la sesión
     setup_session_state()
-    
-    # Opciones del sistema operativo
-    opciones = ["Windows", "macOS", "Linux", "Android"]
-    # Rutas correspondientes a cada sistema operativo
-    usuario = os.getlogin()
-    rutas_descarga = {
-        "Windows": f"C:\\Users\\{usuario}\\Downloads",
-        "macOS": f"/Users/{usuario}/Downloads",
-        "Linux": f"/home/{usuario}/Downloads",
-        "Android": "storage/emulated/0/Download"
-    }
 
     # Entrada para la URL del video de YouTube
     url = st.text_input("Ingrese la URL del video de YouTube:", value=st.session_state.url)
@@ -24,18 +14,17 @@ def main():
     # Selección del formato de descarga
     format_type = st.selectbox("Selecciona el formato de descarga:", ["video", "audio"], index=["video", "audio"].index(st.session_state.format_type))
     
-    RutaFolder = st.selectbox("Selecciones la direccion para guardar el video", opciones)
-    # Obtener la ruta correspondiente según la selección
-    ruta_seleccionada = rutas_descarga.get(RutaFolder, "Ruta no disponible")
+     # Ruta temporal para almacenamiento en Streamlit Cloud
+    ruta_temporal = Path("/tmp/descargas_streamlit")
     try:
-        # Verificar si la ruta existe, si no, crearla
-        if not os.path.exists(ruta_seleccionada):
-            os.makedirs(ruta_seleccionada)
-            st.write(f"Se ha creado la ruta: {ruta_seleccionada}")
+        # Verificar si la ruta temporal existe, si no, crearla
+        if not ruta_temporal.exists():
+            ruta_temporal.mkdir(parents=True, exist_ok=True)
+            st.write(f"Se ha creado la ruta temporal: {ruta_temporal}")
         else:
-            st.write(f"La ruta seleccionada para guardar el video es: {ruta_seleccionada}")
+            st.write(f"Se utilizará la ruta temporal: {ruta_temporal}")
     except Exception as e:
-        st.error(f"Error al crear la ruta: {e}")
+        st.error(f"Error al crear la ruta temporal: {e}")
         return
 
     # Verificar cambios y advertir al usuario si es necesario
