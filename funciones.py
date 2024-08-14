da228572420c7b2d5cde134e042e26ac942c58d8
ColiJD <colindresj9@gmail.com>
2024-08-13 22:50:31 -0600

    Agregando request

--- funciones.py
+++ funciones.py
@@ -1,18 +1,46 @@
 import streamlit as st
 import yt_dlp as ytdlp
+import requests
+
+# Ruta a FFmpeg
+ffmpeg_path = r"C:\ffmpeg\bin"
+
+# Crea una sesión de requests para manejar las cookies de YouTube
+session = requests.Session()
+
+def save_cookies_to_netscape_format(cookies, filename):
+    with open(filename, 'w') as f:
+        f.write("# Netscape HTTP Cookie File\n")
+        f.write("# This is a generated file! Do not edit.\n")
+        for cookie in cookies:
+            # Línea en formato Netscape
+            f.write(f"{cookie.domain}\t{'TRUE' if cookie.domain.startswith('.') else 'FALSE'}\t{cookie.path}\t{'TRUE' if cookie.secure else 'FALSE'}\t{cookie.expires}\t{cookie.name}\t{cookie.value}\n")
+
+def fetch_cookies(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = session.get(url, headers=headers)
+    if response.status_code == 200:
+        save_cookies_to_netscape_format(session.cookies, 'cookies.txt')
+    else:
+        st.error(f'Error al acceder a la URL: {response.status_code}')
 
 class YouTubeDownloader:
-    def __init__(self, url,format_type):
+    def __init__(self, url, format_type):
         self.url = url
-        # self.download_folder = download_folder
         self.format_type = format_type
         self.progress_bar = st.progress(0)
+        
+        # Fetch cookies and save them to a file
+        fetch_cookies(self.url)
+        
         self.ytdlp = ytdlp.YoutubeDL({
-            # 'outtmpl': os.path.join(download_folder, '%(title)s.%(ext)s'),
             'format': 'bestaudio/best' if format_type == 'audio' else 'best',
             'progress_hooks': [self._progress_hook],
-            'cookies': 'cookies.txt',  # Especifica la ruta a tus cookies aquí,
+            'cookiefile': 'cookies.txt',  # Especifica la ruta a tus cookies aquí
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',  # Simula un navegador
+            'ffmpeg_location': ffmpeg_path,  # Ruta a FFmpeg
         })
 
     def _progress_hook(self, d):
@@ -37,22 +65,6 @@ class YouTubeDownloader:
             self.ytdlp.download([self.url])
         except Exception as e:
             st.error(f"Error durante la descarga: {e}")
-
-# def get_folder_path():
-#     try:
-#         result = subprocess.run(["python", "select_folder.py"], capture_output=True, text=True)
-#         return result.stdout.strip()
-#     except Exception as e:
-#         st.error(f"Error al seleccionar la carpeta: {e}")
-#         return None
-
-# def select_folder_button():
-#     if st.session_state.download_folder is None:
-#         if st.button("Seleccionar Carpeta de Destino"):
-#             st.session_state.download_folder = get_folder_path()
-#             st.write(f"Ruta de la carpeta de destino: {st.session_state.download_folder}" 
-#                      if st.session_state.download_folder 
-#                      else "No se seleccionó ninguna carpeta.")
 
 def validate_url_and_format(url, format_type):
     if url:
@@ -80,8 +92,6 @@ def setup_session_state():
     """
     Inicializa y configura las variables de estado de la sesión si no están definidas.
     """
-    # if 'download_folder' not in st.session_state:
-    #     st.session_state.download_folder = None
     if 'validated' not in st.session_state:
         st.session_state.validated = False
     if 'url' not in st.session_state:
